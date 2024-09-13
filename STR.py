8925988804cf14529ed2a9e8e23bf6baf4488801
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2024-09-13 18:17:31 +0200

    Move var-pprint to Pretty.py

--- STR.py
+++ STR.py
@@ -5,6 +5,7 @@ from pyomo.util.infeasible import log_infeasible_constraints
 
 from CaseStudy import CaseStudy
 from LEGO import LEGO
+from Pretty import pprint_var
 
 ########################################################################################################################
 # Setup
@@ -24,48 +25,6 @@ cs = CaseStudy("data/example/", merge_single_node_buses=True)
 ########################################################################################################################
 
 model = LEGO(cs).build_model()
-
-
-# Helper function to pretty-print the values of a Pyomo indexed variable within zone of interest
-def pprint_var(var, zoi, index_positions: list = None, decimals: int = 2):
-    if index_positions is None:
-        index_positions = [0]
-
-    key_list = ["Key"]
-    lower_list = ["Lower"]
-    value_list = ["Value"]
-    upper_list = ["Upper"]
-    fixed_list = ["Fixed"]
-    stale_list = ["Stale"]
-    domain_list = ["Domain"]
-
-    for index in var:
-        # check if at least one index is in zone of interest
-        if not any(i in zoi for i in index):
-            continue
-        key_list.append(str(index))
-        lower_list.append(f"{var[index].lb:.2f}" if var[index].has_lb() else str(var[index].lb))
-        value_list.append(f"{pyo.value(var[index]):.2f}" if not var[index].value is None else str(var[index].value))
-        upper_list.append(f"{var[index].ub:.2f}" if var[index].has_ub() else str(var[index].ub))
-        fixed_list.append(str(var[index].fixed))
-        stale_list.append(str(var[index].stale))
-        domain_list.append(str(var[index].domain.name))
-
-    key_spacer = len(max(key_list, key=len))
-    lower_spacer = len(max(lower_list, key=len))
-    value_spacer = len(max(value_list, key=len))
-    upper_spacer = len(max(upper_list, key=len))
-    fixed_spacer = len(max(fixed_list, key=len))
-    stale_spacer = len(max(stale_list, key=len))
-    domain_spacer = len(max(domain_list, key=len))
-
-    print(f"{var.name} : {var.doc}")
-    print(f"    Size={len(var)}, In Zone of Interest={len(key_list) - 1}, Index={var.index_set()}")
-
-    # Iterate over all lists and print the values
-    for i in range(len(value_list)):
-        print(f"    {key_list[i]:>{key_spacer}} : {lower_list[i]:>{lower_spacer}} : {value_list[i]:>{value_spacer}} : {upper_list[i]:>{upper_spacer}} : {fixed_list[i]:>{fixed_spacer}} : {stale_list[i]:>{stale_spacer}} : {domain_list[i]:>{domain_spacer}}")
-
 
 if __name__ == '__main__':
     from pyomo.opt import SolverFactory
