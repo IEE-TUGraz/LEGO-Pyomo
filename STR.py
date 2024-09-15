d89cc9e44ea40593672891e0c3184e631e2f2035
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2024-09-15 17:39:16 +0200

    Execute multiple case-studies within same execution

--- STR.py
+++ STR.py
@@ -1,6 +1,9 @@
 import logging
+import time
 
+import pandas as pd
 import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
 from pyomo.util.infeasible import log_infeasible_constraints
 
 from CaseStudy import CaseStudy
@@ -18,19 +21,88 @@ pyomo_logger.setLevel(logging.INFO)
 # Data input from case study
 ########################################################################################################################
 
-cs = CaseStudy("data/example/", merge_single_node_buses=True)
+cs = CaseStudy("data/example/", do_not_merge_single_node_buses=True)
 
 ########################################################################################################################
-# Model creation
+# Case study preparation
 ########################################################################################################################
 
-model = LEGO(cs).build_model()
+caseStudies = []
 
-if __name__ == '__main__':
-    from pyomo.opt import SolverFactory
+### All DC-OPF
+csAllDCOPF = cs.copy()
+csAllDCOPF.dPower_Network['Technical Representation'] = 'DC-OPF'
+caseStudies.append(("All DC-OPF", csAllDCOPF))
+
+### Mixed DC-OPF, TP & SN
+csMixed = cs.copy()
+technicalRepresentations = [(("Node_1", "Node_6"), "DC-OPF"),
+                            (("Node_2", "Node_3"), "DC-OPF"),
+                            (("Node_2", "Node_6"), "TP"),
+                            (("Node_3", "Node_4"), "TP"),
+                            (("Node_3", "Node_6"), "SN"),
+                            (("Node_4", "Node_5"), "DC-OPF"),
+                            (("Node_4", "Node_6"), "TP"),
+                            (("Node_4", "Node_9"), "TP"),
+                            (("Node_6", "Node_7"), "DC-OPF"),
+                            (("Node_6", "Node_8"), "SN"),
+                            (("Node_7", "Node_8"), "SN"),
+                            (("Node_8", "Node_9"), "DC-OPF"),
+                            (("Node_1", "Node_4"), "TP")]
+for (i, j), tr in technicalRepresentations:
+    csMixed.dPower_Network.loc[(i, j), 'Technical Representation'] = tr
+csMixed.merge_single_node_buses()
+caseStudies.append(("Mixed DC-OPF, TP & SN", csMixed))
+
+### All TP
+csAllTP = cs.copy()
+csAllTP.dPower_Network['Technical Representation'] = 'TP'
+caseStudies.append(("All TP", csAllTP))
+
+### Mixed TP & SN
+csMixedTPSN = cs.copy()
+technicalRepresentations = [(("Node_1", "Node_6"), "TP"),
+                            (("Node_2", "Node_3"), "TP"),
+                            (("Node_2", "Node_6"), "TP"),
+                            (("Node_3", "Node_4"), "TP"),
+                            (("Node_3", "Node_6"), "SN"),
+                            (("Node_4", "Node_5"), "TP"),
+                            (("Node_4", "Node_6"), "TP"),
+                            (("Node_4", "Node_9"), "TP"),
+                            (("Node_6", "Node_7"), "SN"),
+                            (("Node_6", "Node_8"), "SN"),
+                            (("Node_7", "Node_8"), "SN"),
+                            (("Node_8", "Node_9"), "TP"),
+                            (("Node_1", "Node_4"), "TP")]
+for (i, j), tr in technicalRepresentations:
+    csMixedTPSN.dPower_Network.loc[(i, j), 'Technical Representation'] = tr
+csMixedTPSN.merge_single_node_buses()
+caseStudies.append(("Mixed TP & SN", csMixedTPSN))
+
+### All SN
+csAllSN = cs.copy()
+csAllSN.dPower_Network['Technical Representation'] = 'SN'
+csAllSN.merge_single_node_buses()
+caseStudies.append(("All SN", csAllSN))
+
+########################################################################################################################
+# Evaluation
+########################################################################################################################
+
+resultslist = pd.DataFrame(columns=["Objective Value", "Model building time", "Solving time", "Slack variable sum of demand not served", "Slack variable sum of overproduction"])
+
+for caseName, cs in caseStudies:
+    print(f"\n\n{'=' * 60}\n{caseName}\n{'=' * 60}")
+    startModelBUilding = time.time()
+    model = LEGO(cs).build_model()
+    endModelBuilding = time.time()
+    print(f"Building model for {caseName} took {time.time() - endModelBuilding:.2f} seconds")
 
     opt = SolverFactory("gurobi")
+    startSolve = time.time()
     results = opt.solve(model)
+    endSolve = time.time()
+    print(f"Solving model for {caseName} took {time.time() - endSolve:.2f} seconds")
     results.write()
 
     match results.solver.termination_condition:
@@ -54,6 +126,16 @@ if __name__ == '__main__':
     print("Slack variable sum of overproduction:", sum(model.vSlack_OverProduction[rp, k, i].value if model.vSlack_OverProduction[rp, k, i].value is not None else 0 for rp in model.rp for k in model.k for i in model.i))
 
     print("\nObjective Function Value\n" + '-' * 60)
-    print("Objective value:", pyo.value(model.objective))
+    print("Objective value:", f"{pyo.value(model.objective):>8.2f}")
 
-    print("Done")
+    resultslist.loc[caseName] = {"Objective Value": pyo.value(model.objective),
+                                 "Model building time": endModelBuilding - startModelBUilding,
+                                 "Solving time": endSolve - startSolve,
+                                 "Slack variable sum of demand not served": sum(model.vSlack_DemandNotServed[rp, k, i].value if model.vSlack_DemandNotServed[rp, k, i].value is not None else 0 for rp in model.rp for k in model.k for i in model.i),
+                                 "Slack variable sum of overproduction": sum(model.vSlack_OverProduction[rp, k, i].value if model.vSlack_OverProduction[rp, k, i].value is not None else 0 for rp in model.rp for k in model.k for i in model.i)}
+
+# Print results in pretty table
+print("\n\nResults\n" + '-' * 60)
+print(resultslist)
+
+print("Done")
