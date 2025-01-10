1555828e88cebe7a73454701fe3b533b1492da37
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2025-01-10 15:50:51 +0100

    Adjust bounds for v2ndResUP/DW for investment

--- LEGO/modules/secondReserve.py
+++ LEGO/modules/secondReserve.py
@@ -18,13 +18,13 @@ def add_element_definitions_and_bounds(lego: LEGO):
 
     if hasattr(lego.model, "thermalGenerators"):
         for t in lego.model.thermalGenerators:
-            lego.model.v2ndResUP[:, :, t].setub((lego.model.pMaxProd[t] - lego.model.pMinProd[t]) * lego.model.pExisUnits[t])  # TODO: Add MaxInvestUnits to ExisUnits for bound
-            lego.model.v2ndResDW[:, :, t].setub((lego.model.pMaxProd[t] - lego.model.pMinProd[t]) * lego.model.pExisUnits[t])  # TODO: Add MaxInvestUnits to ExisUnits for bound
+            lego.model.v2ndResUP[:, :, t].setub((lego.model.pMaxProd[t] - lego.model.pMinProd[t]) * (lego.model.pExisUnits[t] + (lego.model.pMaxInvest[t] * lego.model.pEnabInv[t])))
+            lego.model.v2ndResDW[:, :, t].setub((lego.model.pMaxProd[t] - lego.model.pMinProd[t]) * (lego.model.pExisUnits[t] + (lego.model.pMaxInvest[t] * lego.model.pEnabInv[t])))
 
     if hasattr(lego.model, "storageUnits"):
         for s in lego.model.storageUnits:
-            lego.model.v2ndResUP[:, :, s].setub(lego.model.pMaxProd[s] * lego.model.pExisUnits[s])  # TODO: Add MaxInvestUnits to ExisUnits for bound
-            lego.model.v2ndResDW[:, :, s].setub(max(lego.model.pMaxCons[s], lego.model.pMaxProd[s]) * lego.model.pExisUnits[s])  # TODO: Add MaxInvestUnits to ExisUnits for bound
+            lego.model.v2ndResUP[:, :, s].setub(lego.model.pMaxProd[s] * (lego.model.pExisUnits[s] + (lego.model.pMaxInvest[s] * lego.model.pEnabInv[s])))
+            lego.model.v2ndResDW[:, :, s].setub(max(lego.model.pMaxCons[s], lego.model.pMaxProd[s]) * (lego.model.pExisUnits[s] + (lego.model.pMaxInvest[s] * lego.model.pEnabInv[s])))
 
     # Parameters
     lego.model.p2ndResUp = pyo.Param(initialize=lego.cs.dPower_Parameters["p2ndResUp"], doc="2nd reserve factor up")
@@ -34,12 +34,12 @@ def add_element_definitions_and_bounds(lego: LEGO):
 @LEGOUtilities.checkExecutionLog([add_element_definitions_and_bounds])
 def add_constraints(lego: LEGO):
     def e2ReserveUp_rule(model, rp, k):  # TODO: Check if we need to multiply with ExisUnite or InvestedUnits here
-        return sum(model.v2ndResUP[rp, k, t] for t in model.thermalGenerators) + sum(model.v2ndResUP[rp, k, s] for s in model.storageUnits) >= model.p2ndResUp * sum(model.pDemandP[rp, i, k] for i in model.i)
+        return sum(model.v2ndResUP[rp, k, t] for t in model.thermalGenerators) + sum(model.v2ndResUP[rp, k, s] for s in model.storageUnits) >= model.p2ndResUp * sum(model.pDemandP[rp, k, i] for i in model.i)
 
     lego.model.e2ReserveUp = pyo.Constraint(lego.model.rp, lego.model.k, doc="2nd reserve up", rule=e2ReserveUp_rule)
 
     def e2ReserveDw_rule(model, rp, k):
-        return sum(model.v2ndResDW[rp, k, t] for t in model.thermalGenerators) + sum(model.v2ndResDW[rp, k, s] for s in model.storageUnits) >= model.p2ndResDw * sum(model.pDemandP[rp, i, k] for i in model.i)
+        return sum(model.v2ndResDW[rp, k, t] for t in model.thermalGenerators) + sum(model.v2ndResDW[rp, k, s] for s in model.storageUnits) >= model.p2ndResDw * sum(model.pDemandP[rp, k, i] for i in model.i)
 
     lego.model.e2ReserveDw = pyo.Constraint(lego.model.rp, lego.model.k, doc="2nd reserve down", rule=e2ReserveDw_rule)
 
