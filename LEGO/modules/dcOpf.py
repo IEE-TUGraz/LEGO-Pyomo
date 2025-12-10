1149ea80409ff8aa082ac1fb2057bf5a7667c394
Benjamin Stöckl <ben.stoe@gmail.com>
2025-12-10 09:43:55 +0100

    Remove last SOCP constraints from dcOpf.py

--- LEGO/modules/dcOpf.py
+++ LEGO/modules/dcOpf.py
@@ -39,49 +39,49 @@ def add_element_definitions_and_bounds(model: pyo.ConcreteModel, cs: CaseStudy) 
         model.constraintsActiveK = pyo.Set(doc='Timesteps where constraints are active', initialize=model.k)
 
     model.hindex = cs.dPower_Hindex.index
-        # Helper function for creating reverse and bidirectional sets
-        def make_reverse_set(original_set):
-            reverse = []
-            for (i, j, c) in original_set:
-                reverse.append((j, i, c))
-            return reverse
+    # Helper function for creating reverse and bidirectional sets
+    def make_reverse_set(original_set):
+        reverse = []
+        for (i, j, c) in original_set:
+            reverse.append((j, i, c))
+        return reverse
 
-        model.la_reverse = pyo.Set(doc='Reverse lines for la', initialize=lambda m: make_reverse_set(m.la), dimen=3)
-        model.la_no_c = pyo.Set(doc='All lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.la}, dimen=2)
-        model.la_full = pyo.Set(doc='All lines incl. reverse lines', initialize=lambda m: set(m.la) | set(m.la_reverse), dimen=3)
-        model.la_full_no_c = pyo.Set(doc='All lines incl. reverse lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.la_full}, dimen=2)
+    model.la_reverse = pyo.Set(doc='Reverse lines for la', initialize=lambda m: make_reverse_set(m.la), dimen=3)
+    model.la_no_c = pyo.Set(doc='All lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.la}, dimen=2)
+    model.la_full = pyo.Set(doc='All lines incl. reverse lines', initialize=lambda m: set(m.la) | set(m.la_reverse), dimen=3)
+    model.la_full_no_c = pyo.Set(doc='All lines incl. reverse lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.la_full}, dimen=2)
 
-        model.le_reverse = pyo.Set(doc='Reverse lines for le', initialize=lambda m: make_reverse_set(m.le), within=model.la_reverse, dimen=3)
-        model.le_full = pyo.Set(doc='Existing lines incl. reverse lines', initialize=lambda m: set(m.le) | set(m.le_reverse), within=model.la_full, dimen=3)
-        model.le_no_c = pyo.Set(doc='Existing lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.le}, dimen=2)
+    model.le_reverse = pyo.Set(doc='Reverse lines for le', initialize=lambda m: make_reverse_set(m.le), within=model.la_reverse, dimen=3)
+    model.le_full = pyo.Set(doc='Existing lines incl. reverse lines', initialize=lambda m: set(m.le) | set(m.le_reverse), within=model.la_full, dimen=3)
+    model.le_no_c = pyo.Set(doc='Existing lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.le}, dimen=2)
 
-        model.lc_reverse = pyo.Set(doc='Reverse lines for lc', initialize=lambda m: make_reverse_set(m.lc), within=model.la_reverse, dimen=3)
-        model.lc_full = pyo.Set(doc='Candidate lines incl. reverse lines', initialize=lambda m: set(m.lc) | set(m.lc_reverse), within=model.la_full, dimen=3)
-        model.lc_full_no_c = pyo.Set(doc='Candidate lines incl. reverse lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.lc_full}, dimen=2)
-        model.lc_no_c = pyo.Set(doc='Candidate lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.lc}, dimen=2)
+    model.lc_reverse = pyo.Set(doc='Reverse lines for lc', initialize=lambda m: make_reverse_set(m.lc), within=model.la_reverse, dimen=3)
+    model.lc_full = pyo.Set(doc='Candidate lines incl. reverse lines', initialize=lambda m: set(m.lc) | set(m.lc_reverse), within=model.la_full, dimen=3)
+    model.lc_full_no_c = pyo.Set(doc='Candidate lines incl. reverse lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.lc_full}, dimen=2)
+    model.lc_no_c = pyo.Set(doc='Candidate lines without circuit dependency', initialize=lambda m: {(i, j) for (i, j, c) in m.lc}, dimen=2)
 
-        # Helper to get the first circuit for each (i, j) pair
-        df_circuits = cs.dPower_Network.reset_index()
+    # Helper to get the first circuit for each (i, j) pair
+    df_circuits = cs.dPower_Network.reset_index()
 
-        # Sort the DataFrame by the desired circuit order
-        df_circuits["c_str"] = df_circuits["c"].astype(str)
-        ordered_circuits = list(dict.fromkeys(df_circuits["c_str"].tolist()))
-        circuit_order = {c: idx for idx, c in enumerate(ordered_circuits)}
-        df_circuits["c_order"] = df_circuits["c_str"].map(circuit_order)
+    # Sort the DataFrame by the desired circuit order
+    df_circuits["c_str"] = df_circuits["c"].astype(str)
+    ordered_circuits = list(dict.fromkeys(df_circuits["c_str"].tolist()))
+    circuit_order = {c: idx for idx, c in enumerate(ordered_circuits)}
+    df_circuits["c_order"] = df_circuits["c_str"].map(circuit_order)
 
-        # Get the first circuit per (i, j) based on this order
-        first_circuit_map = df_circuits.sort_values("c_order").drop_duplicates(subset=["i", "j"]).set_index(["i", "j"])["c"].to_dict()
-        # todo da kommt der fehler
-        # DEPRECATED: Param 'first_circuit_map' declared with an implicit
-        # domain of 'Any'. The default domain for Param objects is 'Any'.  However, we
-        # will be changing that default to 'Reals' in the future.  If you really intend
-        # the domain of this Paramto be 'Any', you can suppress this warning by
-        # explicitly specifying 'within=Any' to the Param constructor.  (deprecated in
-        # 5.6.9, will be removed in (or after) 6.0) (called from
-        # C:\Users\Stephan\anaconda3\envs\LEGO-Pyomo_env\Lib\site-
-        # packages\pyomo\core\base\indexed_component.py:718)
-        model.first_circuit_map = pyo.Param(model.la_no_c, initialize=first_circuit_map, doc='First circuit for each line (i, j)')
-        model.first_circuit_map_bidir = pyo.Param(model.la_full_no_c, initialize={(i, j): c for (i, j), c in model.first_circuit_map.items()} | {(j, i): c for (i, j), c in model.first_circuit_map.items()}, doc='First circuit for each line (i, j) bidirectional')
+    # Get the first circuit per (i, j) based on this order
+    first_circuit_map = df_circuits.sort_values("c_order").drop_duplicates(subset=["i", "j"]).set_index(["i", "j"])["c"].to_dict()
+    # todo da kommt der fehler
+    # DEPRECATED: Param 'first_circuit_map' declared with an implicit
+    # domain of 'Any'. The default domain for Param objects is 'Any'.  However, we
+    # will be changing that default to 'Reals' in the future.  If you really intend
+    # the domain of this Paramto be 'Any', you can suppress this warning by
+    # explicitly specifying 'within=Any' to the Param constructor.  (deprecated in
+    # 5.6.9, will be removed in (or after) 6.0) (called from
+    # C:\Users\Stephan\anaconda3\envs\LEGO-Pyomo_env\Lib\site-
+    # packages\pyomo\core\base\indexed_component.py:718)
+    model.first_circuit_map = pyo.Param(model.la_no_c, initialize=first_circuit_map, doc='First circuit for each line (i, j)')
+    model.first_circuit_map_bidir = pyo.Param(model.la_full_no_c, initialize={(i, j): c for (i, j), c in model.first_circuit_map.items()} | {(j, i): c for (i, j), c in model.first_circuit_map.items()}, doc='First circuit for each line (i, j) bidirectional')
 
     # Parameters
     model.pDemandP = pyo.Param(model.rp, model.k, model.i, initialize=cs.dPower_Demand['value'], doc='Demand at bus i in representative period rp and timestep k')
@@ -134,10 +134,10 @@ def add_element_definitions_and_bounds(model: pyo.ConcreteModel, cs: CaseStudy) 
     model.vGenP = pyo.Var(model.rp, model.k, model.g, doc='Power output of generator g', bounds=lambda model, rp, k, g: (0, model.pMaxProd[g] * (model.pExisUnits[g] + model.pMaxInvest[g] * model.pEnabInv[g])))
     second_stage_variables += [model.vGenP]
 
-    model.vLineP = pyo.Var(model.rp, model.k, model.la if cs.dPower_Parameters["pEnableSOCP"] else model.la, doc='Power flow from bus i to j', bounds=lambda m, rp, k, i, j, c: (-m.pPmax[i,j,c], m.pPmax[i, j, c]) if (i, j, c) in m.la else (-m.pPmax[j, i, c], m.pPmax[j, i, c]))
+    model.vLineP = pyo.Var(model.rp, model.k, model.la, doc='Power flow from bus i to j', bounds=lambda m, rp, k, i, j, c: (-m.pPmax[i,j,c], m.pPmax[i, j, c]) if (i, j, c) in m.la else (-m.pPmax[j, i, c], m.pPmax[j, i, c]))
     second_stage_variables += [model.vLineP]
 
-    # For each DC-OPF/SOCP "island", set node with highest demand as slack node
+    # For each DC-OPF "island", set node with highest demand as slack node
     dTechnicalReprIslands = pd.DataFrame(index=cs.dPower_BusInfo.index, columns=[cs.dPower_BusInfo.index], data=False)
 
     for index, entry in cs.dPower_Network.iterrows():
@@ -239,26 +239,6 @@ def add_constraints(model: pyo.ConcreteModel, cs: CaseStudy):
 
     model.eDC_LimCanLine2 = pyo.Constraint(model.rp, model.constraintsActiveK, model.lc, doc="Power flow limit reverse direction for candidate lines (for DC-OPF)", rule=eDC_LimCanLine2_rule)
 
-    # FACTS (not yet Implemented) TODO: Add FACTS as a set, add FACTS parameters to nodes i
-    if cs.dPower_Parameters["pEnableSOCP"] == 99999:
-        model.eSOCP_QMinFACTS = pyo.Constraint(model.rp, model.constraintsActiveK, model.facts, doc='min reactive power output of FACTS unit', rule=lambda m, rp, k, i: m.vGenQ[rp, k, i] >= m.pMaxGenQ[i] * (m.pExisUnits[i] + m.vGenInvest[i]))
-        model.eSOCP_QMaxFACTS = pyo.Constraint(model.rp, model.constraintsActiveK, model.facts, doc='max reactive power output of FACTS unit', rule=lambda m, rp, k, i: m.vGenQ[rp, k, i] <= m.pMaxGenQ[i] * (m.pExisUnits[i] + m.vGenInvest[i]))
-
-
-    # define a active and reactive power balance constraint for the slack bus to use the ImExport implementation (only called DC to be consistent with the rest of the model)
-    if cs.dPower_Parameters['pEnablePowerImportExport']:
-
-        def eDC_BalanceP_rule(m, rp, k, i):
-            return (- sum(m.vLineP[rp, k, j, m_con, c] for (j, m_con, c) in m.la if j == i))
-        
-        def eSOCP_ReactivePowerFlow_rule(m, rp, k, i):
-            return (- sum(m.vLineQ[rp, k, j, m_con, c] for (j, m_con, c) in m.la if j == i))
-        
-        model.eDC_BalanceP_expr = pyo.Expression(model.rp, model.constraintsActiveK, model.slack_node, rule=eDC_BalanceP_rule)
-        model.eDC_BalanceP = pyo.Constraint(model.rp, model.constraintsActiveK, model.slack_node, doc='Power balance constraint for each bus', rule=lambda m, rp, k, i: m.eDC_BalanceP_expr[rp, k, i] == 0)
-
-        model.eSOCP_BalanceQ_expr = pyo.Expression(model.rp, model.constraintsActiveK, model.slack_node, rule=eSOCP_ReactivePowerFlow_rule)
-        model.eSOCP_BalanceQ = pyo.Constraint(model.rp, model.constraintsActiveK, model.slack_node, doc='Power balance constraint for each bus', rule=lambda m, rp, k, i: m.eSOCP_BalanceQ_expr[rp, k, i] == 0)
 
     # OBJECTIVE FUNCTION ADJUSTMENT(S)
     first_stage_objective = (sum(model.pFixedCost[i, j, c] * model.vLineInvest[i, j, c] for i, j, c in model.lc) +  # Investment cost of transmission lines
