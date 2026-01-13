e76a1041ef9ff8d9f9c478bdb89f3166fe058838
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2026-01-13 13:56:05 +0100

    Adjust logic of Caller.py to have started, finished and error markers

--- Caller.py
+++ Caller.py
@@ -17,17 +17,26 @@ while True:
 
     found_one = False
     for i, line in enumerate(lines):
+        started_job_flag = f"{args.jobs}.started{i}"
         finished_job_flag = f"{args.jobs}.finished{i}"
-        if not os.path.exists(f"{finished_job_flag}"):
-            open(finished_job_flag, 'w').close()
+        error_job_flag = f"{args.jobs}.error{i}"
+        if not os.path.exists(started_job_flag) and not os.path.exists(finished_job_flag) and not os.path.exists(error_job_flag):
+            with open(started_job_flag, 'w') as f:
+                f.write(line.strip())
             found_one = True
             try:
                 printer.information(f"Executing job {i}: {line.strip()}")
                 os.system(line.strip())
+                with open(finished_job_flag, 'r') as f:
+                    f.write(line.strip())
                 printer.information(f"Finished job {i}")
             except Exception as e:
                 printer.error(f"Error while executing job {i}: {e}")
+                with open(error_job_flag, 'w') as f:
+                    f.write(f"Executed command: {line.strip()}\n")
+                    f.write(f"Error while executing job {i}: {e}")
             break
+
     if not found_one:
         printer.information("No more jobs to execute, exiting.")
         break
